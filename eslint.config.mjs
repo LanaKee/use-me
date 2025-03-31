ede6d043ddb37b0652ff9e9e1874dc54c9cac488
soyoka <55011525+soy0ka@users.noreply.github.com>
2025-03-31 17:10:26 +0900

    chore: update ESLint configuration to integrate Prettier and enhance code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+	{
+		plugins: {
+			prettier: (await import('eslint-plugin-prettier')).default
+		},
+		rules: {
+			'prettier/prettier': 'error'
+		}
+	}
+]
 
-export default eslintConfig;
+export default eslintConfig
